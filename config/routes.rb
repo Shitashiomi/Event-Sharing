88f89bce8caa9f7f23484268c5c60605bcc25e97
vagrant <vagrant@localhost.localdomain>
2020-10-26 10:31:20 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'homes#top'
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+}
   get 'homes/about'
   resources :users,only: [:show,:edit,:update]
   resources :inquities,only: [:new, :create]
